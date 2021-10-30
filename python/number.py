fb44f3be8cc4433d7774dfb5704fb3e4373eb96e
Kishore Kumar Balakrishnan <kishorekumar.balakrishnan@tatacommunications.com>
2021-10-30 21:46:18 +0530

    #master - added the helloworld and number py file

---
+++ python/number.py
@@ -0,0 +1,55 @@
+"""
+Demonstrations of Numbers
+"""
+
+#python has an integer type called int
+print("int")
+print("---")
+print(0)
+print(1)
+print(-3)
+print(7707070709070)
+
+print("")
+
+#python has a real number type called float
+print("float")
+print("-----")
+print(0.0)
+print(7.35)
+print(-43.2)
+
+print("")
+
+#Limited precision (it restricts the decimal number size)
+print("Precision")
+print("---------")
+print(4.1234567890123456789)  #output 4.123456789012345
+print(1.2252525252525252)
+
+print("")
+
+#Scientific / Exponential Notation
+print("Scientific Notation")
+print("-------------------")
+print(5e32)
+print(99999999999999999999.2)
+
+print("")
+
+#Infinity
+print("Infinity")
+print("--------")
+print(1e500)
+print(-1e500)
+
+print("")
+
+#Conversions
+print("Conversions between numeric types")
+print("---------------------------------")
+print(float(3))
+print(float(99999999999999999999)) 
+print(int(3.0))
+print(int(3.7))
+print(int(-3.7))
