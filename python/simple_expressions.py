f1ffaee7612ee0073f101cb77e3d4ac8fcad1213
Kishore Kumar Balakrishnan <kishorekumar.balakrishnan@tatacommunications.com>
2021-10-31 13:44:55 +0530

    #master - added the simple expressin

---
+++ python/simple_expressions.py
@@ -0,0 +1,54 @@
+"""
+Demonstration of simple arithmetic expression
+"""
+
+#unary + and -
+print("Unary Operators")
+print("---------------")
+print(+3)
+print(-5)
+print(+7.86)
+print(-3348.63)
+
+print("")
+
+#simple arithmetic
+print("Addition and Subraction")
+print("---------------")
+print(1 + 2)
+print(48 - 89)
+print(3.45 + 2.7)
+print(87.33884 - 12.35)
+print(3 + 6.7)
+print(9.8 - 4)
+
+print("")
+
+#Multiplication
+print("Multiplication")
+print("---------------")
+print(3 * 2)
+print(7.8 * 27.34)
+print(7 * 8.2)
+
+print("")
+
+#Division
+print("Division")
+print("---------------")
+print(8/2)
+print(3/2)
+print(7.536 / 14.3)
+print(8 // 2)
+print(3 // 2)
+print(7.538 // 14.3)
+
+print("")
+
+#Exponentiation
+print("Exponentiation")
+print("---------------")
+print(3 ** 2)
+print(5 ** 4)
+print(32.6 ** 7)
+print(9 ** 0/5)
