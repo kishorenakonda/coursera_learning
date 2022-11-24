272c71400adbb8bf90e65574e31c42d8da1635fe
Kishore Kumar Balakrishnan <kishore.kumar.balakrishnan@oracle.com>
2022-11-24 21:20:12 +0530

    Node callbacks and error handling

--- serverside_nodejs/node-examples/index.js
+++ serverside_nodejs/node-examples/index.js
@@ -9,13 +9,27 @@ var rect = require('./rectangle');
 
 function solveRectangle(l, b) {
     console.log("<-- Solving for rectangle with l =" + l + " and b =" + b + "-->");
-    if (l <= 0 || b <= 0) {
-        console.log("<-- Rectangle dimensions should be greater than zero l =" + l + " and b =" + b + "-->");
-    } else {
-        console.log("The area of the rectangle is " + rect.area(l, b));
-        console.log("The Perimeter of the rectangle is " + rect.perimeter(l, b));
-    }
-    console.log("\n");
+    // if (l <= 0 || b <= 0) {
+    //     console.log("<-- Rectangle dimensions should be greater than zero l =" + l + " and b =" + b + "-->");
+    // } else {
+    //     console.log("The area of the rectangle is " + rect.area(l, b));
+    //     console.log("The Perimeter of the rectangle is " + rect.perimeter(l, b));
+    // }
+    // console.log("\n");
+
+    // Using Callbacks
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("ERROR: ", err.message);
+        }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
 }
 
 solveRectangle(2, 4);
