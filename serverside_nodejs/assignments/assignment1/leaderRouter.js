c51e072462bfaccea93e26b37c3fb14255cce538
Kishore Kumar Balakrishnan <kishore.kumar.balakrishnan@oracle.com>
2022-11-26 17:40:42 +0530

    Completed Assignment 1

--- serverside_nodejs/assignments/assignment1/leaderRouter.js
+++ serverside_nodejs/assignments/assignment1/leaderRouter.js
@@ -1,50 +1,50 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter = express.Router();
+const leaderRouter = express.Router();
 
-dishRouter.use(bodyParser.json());
+leaderRouter.use(bodyParser.json());
 
-dishRouter.route('/')
+leaderRouter.route('/')
     .all((request, response, next) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         next();
     })
     .get((request, response, next) => {
-        response.end('Will send all the dishes to you!');
+        response.end('Will send all the Leaders Information to you!');
     })
     .post((request, response, next) => {
-        response.end('Will add the dish: ' + request.body.name + ' with details: ' + request.body.description);
+        response.end('Will add the Leader with name : ' + request.body.name + ' and with details: ' + request.body.description);
     })
     .put((request, response, next) => {
         response.statusCode = 403;
-        response.end('PUT operation not supported on /dishes');
+        response.end('PUT operation not supported on /leaders');
     })
     .delete((request, response, next) => {
-        response.end('Deleting all dishes');
+        response.end('Deleting all Leaders');
     });
 
-dishRouter.route('/:dishId')
+leaderRouter.route('/:leaderId')
     .all((request, response, next) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         next();
     })
     .get((request, response, next) => {
-        response.end('Will Send details of the dish : ' + request.params.dishId + ' to you');
+        response.end('Will Send details of the Leader : ' + request.params.leaderId + ' to you');
     })
     .post((request, response, next) => {
         response.statusCode = 403;
-        response.end('POST Operation is not supported on /dishes/' + request.params.dishId);
+        response.end('POST Operation is not supported on /leaders/' + request.params.leaderId);
     })
     .put((request, response, next) => {
-        response.write('Updating the dish : ', request.params.dishId + '\n');
-        response.end('Will Update the dish : ' + request.body.name +
+        response.write('Updating the Leader : ', request.params.leaderId + '\n');
+        response.end('Will Update the Leader : ' + request.body.name +
             ' with details : ' + request.body.description);
     })
     .delete((request, response, next) => {
-        response.end('Deleting dish : ' + request.params.dishId);
+        response.end('Deleting Leader : ' + request.params.leaderId);
     });
 
-module.exports = dishRouter;
+module.exports = leaderRouter;
--- serverside_nodejs/node-express/routes/promoRouter.js
+++ serverside_nodejs/node-express/routes/promoRouter.js
@@ -1,50 +1,51 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const dishRouter = express.Router();
+const promoRouter = express.Router();
 
-dishRouter.use(bodyParser.json());
+promoRouter.use(bodyParser.json());
 
-dishRouter.route('/')
+promoRouter.route('/')
     .all((request, response, next) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         next();
     })
     .get((request, response, next) => {
-        response.end('Will send all the dishes to you!');
+        response.end('Will send all the Promotions to you!');
     })
     .post((request, response, next) => {
-        response.end('Will add the dish: ' + request.body.name + ' with details: ' + request.body.description);
+        response.end('Will add the promotion: ' + request.body.name + ' with details: ' + request.body.description);
     })
     .put((request, response, next) => {
         response.statusCode = 403;
-        response.end('PUT operation not supported on /dishes');
+        response.end('PUT operation not supported on /promotions');
     })
     .delete((request, response, next) => {
-        response.end('Deleting all dishes');
+        response.end('Deleting all promotions');
     });
 
-dishRouter.route('/:dishId')
+
+promoRouter.route('/:promoId')
     .all((request, response, next) => {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         next();
     })
     .get((request, response, next) => {
-        response.end('Will Send details of the dish : ' + request.params.dishId + ' to you');
+        response.end('Will Send details of the Promotion : ' + request.params.promoId + ' to you');
     })
     .post((request, response, next) => {
         response.statusCode = 403;
-        response.end('POST Operation is not supported on /dishes/' + request.params.dishId);
+        response.end('POST Operation is not supported on /prmotions/' + request.params.promoId);
     })
     .put((request, response, next) => {
-        response.write('Updating the dish : ', request.params.dishId + '\n');
-        response.end('Will Update the dish : ' + request.body.name +
+        response.write('Updating the Promotion : ', request.params.promoId + '\n');
+        response.end('Will Update the Promotion : ' + request.body.name +
             ' with details : ' + request.body.description);
     })
     .delete((request, response, next) => {
-        response.end('Deleting dish : ' + request.params.dishId);
+        response.end('Deleting Promotion : ' + request.params.promoId);
     });
 
-module.exports = dishRouter;
+module.exports = promoRouter;
