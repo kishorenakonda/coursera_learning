8ec60cf08c1d26edaf34289848392add55957ed7
Kishore Kumar Balakrishnan <kishore.kumar.balakrishnan@oracle.com>
2022-11-26 17:19:10 +0530

    Express Router

--- serverside_nodejs/node-express/index.js
+++ serverside_nodejs/node-express/index.js
@@ -10,35 +10,39 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-const dishes = '/dishes';
-const dishApiUrl = dishes + '/:dishId';
+// Fetching the APIs from the external file / routes
+const dishRouter = require('./routes/dishRouter');
+app.use('/dishes', dishRouter);
+
+// const dishes = '/dishes';
+// const dishApiUrl = dishes + '/:dishId';
 
 // API Request
-app.all(dishApiUrl, (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+// app.all(dishApiUrl, (req, res, next) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     next();
+// });
 
-app.get(dishApiUrl, (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you');
-});
+// app.get(dishApiUrl, (req, res, next) => {
+//     res.end('Will send details of the dish: ' + req.params.dishId + ' to you');
+// });
 
-app.post(dishApiUrl, (req, res, next) => {
-    // res.statusCode = 403;
-    // res.end('POST Operation not supported on ' + dishes + '/' + req.params.dishId);
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
+// app.post(dishApiUrl, (req, res, next) => {
+//     // res.statusCode = 403;
+//     // res.end('POST Operation not supported on ' + dishes + '/' + req.params.dishId);
+//     res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+// });
 
-app.put(dishApiUrl, (req, res, next) => {
-    // res.end('PUT Operation not supported on ' + dishes + '/' + req.params.dishId);
-    res.write('Updating the dish: ' + req.params.dishId);
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
+// app.put(dishApiUrl, (req, res, next) => {
+//     // res.end('PUT Operation not supported on ' + dishes + '/' + req.params.dishId);
+//     res.write('Updating the dish: ' + req.params.dishId);
+//     res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+// });
 
-app.delete(dishApiUrl, (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
+// app.delete(dishApiUrl, (req, res, next) => {
+//     res.end('Deleting dish: ' + req.params.dishId);
+// });
 
 // With this we can serve the static HTML files using express
 app.use(express.static(__dirname + '/public'));
